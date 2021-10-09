7088ef22a469feca69ed4f28a58414a78f790438
akhileshwar735 <48098139+akhileshwar735@users.noreply.github.com>
2021-10-09 21:58:34 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+cd ./employee-service
+mvn clean package -DskipTests
+docker build -t employee-service:latest .
+cd ..
+
+
+cd ./eureka-service
+mvn clean package -DskipTests
+docker build -t eureka-service:latest .
+cd ..
+
+
+cd ./product-service
+mvn clean package -DskipTests
+docker build -t product-service:latest .
+cd ..
+
+cd ./customer-service
+mvn clean package -DskipTests
+docker build -t customer-service:latest .
+cd ..
+
+cd ./gateway-service
+mvn clean package -DskipTests
+docker build -t gateway-service:latest .
+cd ..
+
+cd ./order-service
+mvn clean package -DskipTests
+docker build -t order-service:latest .
+cd ..
